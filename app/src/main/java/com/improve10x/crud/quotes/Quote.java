4d628542a9bef2ff2d49bd6ec329d724454c8e8f
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2022-12-14 15:55:00 +0530

    implemented setData QuotesActivity

--- app/src/main/java/com/improve10x/crud/quotes/Quote.java
+++ app/src/main/java/com/improve10x/crud/quotes/Quote.java
@@ -1,11 +1,22 @@
 package com.improve10x.crud.quotes;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class Quote implements Serializable {
+    @SerializedName("_id")
+    String id;
 
-    String quote;
+    @SerializedName("quote")
+    String quoteText;
+
+    @SerializedName("authorName")
     String authorName;
+
+    @SerializedName("category")
     String category;
+
+    @SerializedName("imageUrl")
     String imageUrl;
 }
--- app/src/main/java/com/improve10x/crud/quotes/QuotesActivity.java
+++ app/src/main/java/com/improve10x/crud/quotes/QuotesActivity.java
@@ -16,5 +16,24 @@ public class QuotesActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotes);
         getSupportActionBar().setTitle("Quotes");
+        setupData();
+    }
+
+    private void setupData() {
+        quotes = new ArrayList<>();
+
+        Quote quoteTxt = new Quote();
+        quoteTxt.quoteText = "Your FUTURE is created by what you do TODAY";
+        quoteTxt.authorName = "";
+        quoteTxt.category = "career";
+        quoteTxt.imageUrl = "https://www.kochiesbusinessbuilders.com.au/wp-content/uploads/2022/02/motivational-quote.jpg";
+        quotes.add(quoteTxt);
+
+        Quote quoteTxt1 = new Quote();
+        quoteTxt1.quoteText = "No matter how many mistakes\nyou make or how slow your progress,\nyou are still way ahead of\neveryone who is't trying";
+        quoteTxt1.authorName = "Tony Robbins";
+        quoteTxt1.category = "progress";
+        quoteTxt1.imageUrl = "https://emilysquotes.files.wordpress.com/2014/03/emilysquotes-com-mistakes-progress-slow-trying-tony-robbins-inspirational-motivational.jpg?w=788";
+        quotes.add(quoteTxt1);
     }
 }
