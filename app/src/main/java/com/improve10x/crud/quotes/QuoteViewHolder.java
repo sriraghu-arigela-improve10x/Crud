730f328f82f5dd9f2819e541e48f27eada3654b6
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2022-12-14 16:21:02 +0530

    created java class QuoteViewHolder &  create quote_item.xml

---
+++ app/src/main/java/com/improve10x/crud/quotes/QuoteViewHolder.java
@@ -0,0 +1,24 @@
+package com.improve10x.crud.quotes;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.improve10x.crud.R;
+
+public class QuoteViewHolder extends RecyclerView.ViewHolder {
+    ImageView quoteTextTxt;
+    TextView authorNameTxt;
+    TextView quoteImageBtn;
+
+    public QuoteViewHolder(@NonNull View itemView) {
+        super(itemView);
+        quoteTextTxt = itemView.findViewById(R.id.quote_text_txt);
+        authorNameTxt = itemView.findViewById(R.id.authornname_txt);
+        quoteImageBtn = itemView.findViewById(R.id.quote_image_btn);
+
+    }
+}
