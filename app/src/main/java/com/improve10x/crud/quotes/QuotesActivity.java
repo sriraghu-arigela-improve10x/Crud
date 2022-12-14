933a5725a211d8139a41621c98f0726c706d7081
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2022-12-14 14:53:59 +0530

    Created new quotes package in Crud Project

---
+++ app/src/main/java/com/improve10x/crud/quotes/QuotesActivity.java
@@ -0,0 +1,16 @@
+package com.improve10x.crud.quotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.improve10x.crud.R;
+
+public class QuotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quotes);
+    }
+}
